90ce603586df7498f5af6358f542cc40d1ff7853
Sacha Sedriks <sachased@gmail.com>
2025-03-26 13:46:45 -0700

    Establish tabla rasa boilerplate for exercise

---
+++ README.md
@@ -0,0 +1,20 @@
+# What?
+
+March 2025.
+Revisiting my Rocket Travel coding interview exercise
+taken during my May 2020 job interview (which I recorded, btw)
+
+Task(s):
+- [ ] Parse the data
+- [ ] Component #1: Inventory List via data 
+    - (each list item with a button to add to shopping cart)
+- [ ] Component #2: Shopping Cart List
+- [ ] Functionality: if user adds inventory item to cart it should:
+    - if not yet in the List: 
+        - add item to list
+    - if already in the list
+        - update the quantity
+    - update Total Price
+
+... all within an hour.
+
---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+// import React from "react";
+import "./App.css";
+
+/**
+ * March 2025
+ * Revisiting my Rocket Travel coding interview exercise
+ * taken during my May 2020 job interview (and recorded)
+ *
+ * Task(s):
+ * - [ ] Parse the data
+ * - [ ] Component #1: Inventory List via data (each with a button to add to shopping cart)
+ * - [ ] Component #2: Shopping Cart List
+ * - Functionality: if user adds inventory item to cart it should:
+ * ---- if not yet in the List: add item to list
+ * ---- if already in the list, update the quantity
+ * ---- update Total Price
+ *
+ * ... all within an hour.
+ */
+
+export default function App() {
+  return <div className="App">Hello World</div>;
+}
